822d49970f1ee186cfb359bf1157d5d42bbf7729
iolowookere217 <olowookere.isaaco@gmail.com>
2023-10-01 03:43:59 +0000

    pascal triangle

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def pascal_triangle(n):
+    triangle = []
+    for i in range(n):
+        row = []
+        for j in range(i+1):
+            if (j == 0) or (j == i):
+                row.append(1)
+            else:
+                row.append(triangle[i-1][j] + triangle[i-1][j-1])
+        triangle.append(row)
+    return triangle
