1c1d80d1292d9ae8513b42a56bc7c66c92e71ccf
iolowookere217 <olowookere.isaaco@gmail.com>
2023-10-19 21:28:04 +0000

    testing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,46 +1,40 @@
 #!/usr/bin/python3
-"""A script for parsing HTTP request logs from stdin
-"""
-
+"""This module reads stdin line by line and computes metrics"""
 import sys
 
-status_code_list = [200, 301, 400, 401, 403, 404, 405, 500]
+possible_status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
+lines_read = 0
+status_codes_map = {}
 total_file_size = 0
-number_of_lines = 0
-status_code_map = {}
 
 
 def print_stats():
-    """handles the printing out of the statistics"""
-    print(f"File Size: {total_file_size}")
-    for status, count in sorted(status_code_map.items()):
-        print(f"{status}: {count}")
+    """prints out the statistics"""
+    print("File size: {}".format(total_file_size))
+    for status, count in sorted(status_codes_map.items()):
+        print("{}: {}".format(status, count))
 
 
 try:
     for line in sys.stdin:
+        line_tokens = line.split()
         try:
-            line = line.split()
-            fileSize = int(line[-1])
-            total_file_size += fileSize
-            statusCode = int(line[-2])
-
-            if statusCode in status_code_list:
-                if statusCode in status_code_map:
-                    status_code_map[statusCode] += 1
+            file_size = int(line_tokens[-1])
+            total_file_size += file_size
+            status_code = int(line_tokens[-2])
+            if status_code in possible_status_codes:
+                if status_code in status_codes_map:
+                    status_codes_map[status_code] += 1
                 else:
-                    status_code_map[statusCode] = 1
-
-                number_of_lines += 1
-
-                if (number_of_lines % 10) == 0:
-                    print_stats()
-
+                    status_codes_map[status_code] = 1
         except ValueError:
             pass
+        lines_read += 1
+        if lines_read % 10 == 0:
+            print_stats()
 
-    if (number_of_lines == 0) or (number_of_line % 10 != 0):
+    if (lines_read == 0) or (lines_read % 10 != 0):
         print_stats()
 
-except KeyboardInterrupt:
+except (KeyboardInterrupt):
     print_stats()
