3f44e5837ab19bb602033daf1761f4b7f79fede0
iolowookere217 <olowookere.isaaco@gmail.com>
2023-10-26 18:12:54 +0000

    testing

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,33 +1,26 @@
 #!/usr/bin/python3
-"""This module defines a function validUTF8
-
-        UTF8 Format:
-
-        1-byte Sequence: 0xxxxxxx;
-        2-byte Sequence: 110xxxxx 10xxxxxx;
-        3-byte Sequence: 1110xxxx 10xxxxxx 10xxxxxx;
-        4-byte Sequence: 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx;
-    """
+""" Defines a method to check UTF8 Valid integers """
 
 
 def validUTF8(data):
-    "convertion to binary and checking validation condition"
-    byts_remaining = 0
-    for item in data:
-        binary_char = format(item, '08b')
-        if byts_remaining == 0:
-            if binary_char.startswith("0"):
+    """ Checks UTF8 Valid data set """
+    no_bytes_rem = 0
+    for byt in data:
+        bin_char = format(byt, '08b')
+        if no_bytes_rem == 0:
+            if bin_char.startswith('0'):
                 continue
-            elif binary_char.startswith("110"):
-                byts_remaining = 1
-            elif binary_char.startswith("1110"):
-                byts_remaining = 2
-            elif binary_char.startswith("11110"):
-                byts_remaining = 3
+            elif bin_char.startswith('110'):
+                no_bytes_rem = 1
+            elif bin_char.startswith('1110'):
+                no_bytes_rem = 2
+            elif bin_char.startswith('11110'):
+                no_bytes_rem = 3
+            else:
+                return False
+        else:
+            if bin_char.startswith('10'):
+                no_bytes_rem -= 1
+                continue
             return False
-
-        if binary_char.startswith("10"):
-            byts_remaining -= 1
-            continue
-        return False
     return True
